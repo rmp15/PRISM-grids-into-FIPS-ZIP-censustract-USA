7b5ed64fabc44837ec7c0871c35f964bc2f6a83c
rmp15 <r.parks15@imperial.ac.uk>
2022-01-27 16:17:48 -0500

    updated to run on med-bio cluster

--- prog/02_grid_county_intersection/grid_county_intersection_raster_prism_medbio.R
+++ prog/02_grid_county_intersection/grid_county_intersection_raster_prism_medbio.R
@@ -12,6 +12,7 @@ rm(list=ls())
 # 1a Declare root directory, folder locations and load essential stuff
 project.folder = paste0(print(here::here()),'/')
 
+library(data.table)
 library(maptools)
 library(mapproj)
 library(rgeos)
@@ -61,74 +62,6 @@ if(space.res=='fips'){
 # get projection of shapefile
 original.proj = proj4string(us.national)
 
-################################################################
-# function to perform analysis for entire state
-state.analysis = function(state.arg='01',output=0) {
-
-    # isolate state
-    state.fips  = as.character(state.arg)
-    us.state = us.main[us.main$STATEFP %in% state.fips,]
-
-    # plot state with highlighted county and grids that overlap
-    if(output==1){
-        pdf(paste0(dir.output,'county_graticule_highlighted_unproj_',state.fips,'.pdf'))}
-
-    if(space.res=='zip'){
-        # obtain a list of zip codes in a particular state
-        zips = sort(unique(as.character(us.state$ZCTA5CE00)))
-        # print(zips)
-    }
-    if(space.res=='fips'){
-        # obtain a list of fips codes in a particular state
-        zips = sort(unique(as.character(us.state$GEOID)))
-        # print(zips)
-    }
-
-    # create empty dataframe to fill with zip code summary information
-    weighted.area = data.frame()
-
-    for(zip in zips) {
-
-        # process zip preamble
-        zip      = as.character(zip)
-        # print(paste('current status:',zip))
-
-        # isolate zip to highlight
-        if(space.res=='zip'){us.zip = us.state[us.state$ZCTA5CE00 %in% zip,]}
-        if(space.res=='fips'){us.zip = us.state[us.state$GEOID %in% zip,]}
-
-        # crop raster by current zip code
-        # raster.crop = crop(x=raster.full,y=us.zip)
-        ## raster.crop = crop(x=raster.full,y=us.zip) TO PUT BACK IN IF USING BELOW
-
-        # to plot if want to check that raster and shapefile overlap using manhattan
-        # plot(manhattan) ; plot(raster.manhattan,add=TRUE) ; plot(manhattan,add=TRUE)
-        # plot(raster.crop,add=TRUE, col='grey') ;  plot(us.zip,col='red',add=TRUE)
-
-        # create polygon from cropped polygon
-        ## raster.crop[raster.crop[]<0] = NA TO PUT BACK IN IF USING BELOW
-
-        # create weighted average of values from raster polygon
-        # from https://gis.stackexchange.com/questions/213493/area-weighted-average-raster-values-within-each-spatialpolygonsdataframe-polygon/213503#213503
-        current.value = extract(x=raster.full,weights = TRUE, normalizeWeights=TRUE,y=us.zip,fun=mean,df=TRUE,na.rm=TRUE)
-
-        to.add = data.frame(zip,value=current.value[1,2])
-        weighted.area = rbind(weighted.area,to.add)
-
-        # print(to.add)
-    }
-
-    names(weighted.area) = c('zip',dname)
-
-    return(weighted.area)
-
-}
-
-################################################################
-
-# record of every state in the current shapefile
-states = sort(unique(as.character(us.main$STATEFP)))
-
 # load raster and make same projection as zip code map
 if(time.res=='annual'){
     raster.full = raster(paste0('~/data/climate/prism/asc/PRISM_',dname,'_stable_4kmM3_',year,'_all_asc/PRISM_',dname,'_stable_4kmM3_',year,'_asc.asc'))
@@ -157,25 +90,20 @@ if(time.res=='daily'){
         # load raster for relevant date
         raster.full = raster(paste0('~/data/climate/prism/bil/PRISM_',dname,'_stable_4kmD2_',year,'0101_',year,'1231_bil/PRISM_',dname,'_stable_4kmD2_',year,day.month,'_bil.bil'))
         raster.full = projectRaster(raster.full, crs=original.proj)
+        
+        # perform over entire of mainland USA
+        weighted.area.national  = extract(x=raster.full,weights = TRUE, normalizeWeights=TRUE,y=us.main,fun=mean,df=TRUE,na.rm=TRUE)
+        weighted.area.national = data.frame(zip=paste0(us.main$STATEFP,us.main$COUNTYFP), weighted.area.national[,2])
+        weighted.area.national = weighted.area.national[order(weighted.area.national$zip),]
+        rownames(weighted.area.national) = NULL
+        names(weighted.area.national) = c('zip',dname)
+        
+        weighted.area.national$date = format(as.Date(date), "%d/%m/%Y")
+        weighted.area.national$day = day
+        weighted.area.national$month = month
+        weighted.area.national$year = year
 
-        # create empty dataframe to fill with zip code summary information
-        weighted.area.national = data.frame()
-
-        # perform loop across all states
-        system.time(
-        for(i in states){
-        analysis.dummy = state.analysis(i)
-        analysis.dummy$date = format(as.Date(date), "%d/%m/%Y")
-        analysis.dummy$day = day
-        analysis.dummy$month = month
-        analysis.dummy$year = year
-
-        weighted.area.national = rbind(weighted.area.national,analysis.dummy)
-        }
-        )
-
-        # weighted.area.national = weighted.area.national[,c(3,1,2)]
-        weighted.area.national.total = rbind(weighted.area.national.total,weighted.area.national)
+        weighted.area.national.total = data.table::rbindlist(list(weighted.area.national.total,weighted.area.national))
     }
 }
 
@@ -187,11 +115,3 @@ if(space.res=='zip'){
 if(space.res=='fips'){
     saveRDS(weighted.area.national.total,paste0(dir.output,'weighted_area_raster_fips_',dname,'_',time.res,'_',as.character(year),'.rds'))
 }
-
-# LEGACY BELOW
-
-# if desired for manhattan test if looking at New York
-# if(space.res=='zip'){
-#     manhattan = subset(us.main,ZCTA5CE00%in%c(10001:10286))
-#     raster.manhattan = crop(x=raster.full,y=manhattan)
-# }
