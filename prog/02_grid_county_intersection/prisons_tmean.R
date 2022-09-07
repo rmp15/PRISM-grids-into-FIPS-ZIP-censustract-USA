25f167024908c0c554935db089edc7ad1bb89fc6
rmp15 <r.parks15@imperial.ac.uk>
2022-09-07 18:41:04 -0400

    added edited prison shapefile

--- prog/02_grid_county_intersection/prisons_tmean.R
+++ prog/02_grid_county_intersection/prisons_tmean.R
@@ -13,7 +13,7 @@ rm(list=ls())
 args = commandArgs(trailingOnly=TRUE)
 
 # year of interest
-year = as.numeric(args[1]) # 2000
+year = as.numeric(args[1])
 dname = 'tmean'
 time.res = 'daily'
 space.res = 'prison'
